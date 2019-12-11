58fea7c4df241c929efe4121d1189e1bd56d525a
Rachel Bear <rachel.j.annen@gmail.com>
2019-12-11 15:08:44 -0500

    week 2 debugging activity complete

--- Week2/debugging.py
+++ Week2/debugging.py
@@ -9,3 +9,19 @@ table_dimensions_feet = {
 
 table_length_inches = feet_to_inches(table_dimensions_feet["length"])
 print(table_length_inches)
+
+
+price = 20, 6, 9, 14, 5
+
+hst = 0.13
+
+def get_total(price, tax_percent):
+	total = price * (tax_percent + 1)
+	total_rounded = round(total, 2)
+	return total_rounded
+
+first_total = get_total(price[0], hst)
+second_total = get_total(price[1], hst)
+
+print("The first two totals are $" + str(first_total) + "and $" + str(second_total) + "respectively.")
+#The first two totals are $22.6 and $6.78 respectively.
